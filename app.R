56a779a51744172d49ecfefd5535d01662746511
juliendurand <julien.durand.1981@gmail.com>
2016-09-20 14:01:52 +0200

    update to take volume into account

--- app.R
+++ app.R
@@ -15,7 +15,7 @@ mydata <-  read.csv("data/quotes.csv")
 levels(mydata$CONVERSION) <-  c(levels(mydata$CONVERSION), "ANI")
 mydata$CONVERSION[mydata$CONVERSION==""] <- "ANI"
 mydata$DATE_EMISS <- as.Date(mydata$DATE_EMISS, "%d/%m/%Y")
-mydata <- mydata[mydata$DATE_EMISS >= as.Date("01/03/2015", "%d/%m/%Y") &
+mydata <- mydata[mydata$DATE_EMISS >= as.Date("01/02/2015", "%d/%m/%Y") &
                    mydata$DATE_EMISS <= as.Date("01/01/2016", "%d/%m/%Y"), ]
 
 st <- function(df, dept=NA, region=NA){
@@ -26,48 +26,49 @@ st <- function(df, dept=NA, region=NA){
     df <- df[df$REGION == region, ]
   }
   df <- dcast(df, MOIS+ANNEE~CONVERSION, length, value.var="USERID")
-  df$CONVERTED <- df$AND + df$ANI
-  df$TOTAL <- df$AND + df$ANI + df$DNT
+  df$CONVERTED <- df$AND + df$DT
+  df$TOTAL <- df$AND + df$DT + df$DNT
   df$CONVERSION <- df$CONVERTED / df$TOTAL
   df$DATE <- as.Date(ISOdate(df$ANNEE, df$MOIS, 1))
   return(df)
 }
 
-
-# Define UI for application that draws a histogram
+# Define UI for application
 ui <- shinyUI(fluidPage(
-
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-
-   # Sidebar with a slider input for number of bins
-   sidebarLayout(
-      sidebarPanel(
-        textInput("deptInputId", "Departement", value = ""),
-        textInput("regionInputId", "Region", value = "")
-      ),
-
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
-))
+  titlePanel("AXA France IARD PP AGA - Taux de Transformation"),
+  textInput("deptInputId", "Departement", value = ""),
+  textInput("regionInputId", "Region", value = ""),
+  plotOutput("distPlot"),
+  plotOutput("volumePlot")
+  )
+)
 
 # Define server logic required to draw a histogram
 server <- shinyServer(function(input, output) {
-
-   output$distPlot <- renderPlot({
-     print(input)
-     data <- st(mydata, dept=input$deptInputId, region=input$regionInputId)
-     ggplot(data=data, aes(DATE, CONVERSION)) +
-       geom_line() +
-       xlab("Monthly View") +
-       scale_x_date(date_labels = "%b-%Y") +
-       ylab("Conversion Rate") +
-       ylim(0, 1) +
-       theme_bw()
-   })
+  
+  data <- reactive({
+    st(mydata, dept=input$deptInputId, region=input$regionInputId)
+  }) 
+  
+  output$distPlot <- renderPlot({  
+    ggplot(data=data(), aes(DATE, CONVERSION)) +
+      geom_line() +
+      xlab("Monthly View") +
+      #scale_x_date(date_labels = "%b-%Y") +
+      ylab("Conversion Rate") +
+      ylim(0.25, 0.75) +
+      theme_bw()
+  })
+  
+  output$volumePlot <- renderPlot({
+    ggplot(data=data(), aes(DATE, TOTAL)) +
+      geom_line() +
+      xlab("Monthly View") +
+      expand_limits(y = 0) +
+      #scale_x_date(date_labels = "%b-%Y") +
+      ylab("Volume") +
+      theme_bw()
+  })
 })
 
 # Run the application
